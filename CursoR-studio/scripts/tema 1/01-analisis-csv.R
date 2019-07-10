db442aad28e8b0138b8f31a1c5ea4e99ca0a95a4
cesarramoslc <43019992+cesarramoslc@users.noreply.github.com>
2019-07-10 11:37:24 -0500

    Create 01-analisis-csv.R

---
+++ CursoR-studio/scripts/tema 1/01-analisis-csv.R
@@ -0,0 +1,8 @@
+
+auto <- read.csv("https://raw.githubusercontent.com/joanby/r-course/master/data/tema1/auto-mpg.csv" , header = T, sep = ",")
+names(auto)
+
+#read.csv2() == read.csv("filename", sep =";", dec =",")
+# sep="\t"
+
+
